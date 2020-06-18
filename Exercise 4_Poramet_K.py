8c89e76ec7319df690cb63a22e3c25b77fb8ba3e
ANAS <pawarate31@gmail.com>
2020-06-18 08:53:49 +0700

    Exercise 4

---
+++ Exercise 4_Poramet_K.py
@@ -0,0 +1,9 @@
+Eng = 60
+General = 70.8
+Comsystem = 89
+Compro = 79
+print("---Your Score---")
+print("Foundation English :",Eng)
+print("General Business : ",General)
+print("Introduction to Computer Systems :",Comsystem)
+print("Computer Programming : ",Compro)
