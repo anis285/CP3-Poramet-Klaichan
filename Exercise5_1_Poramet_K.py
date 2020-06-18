a801c6a4a2e8864167151dea9dc83bb4b1fca1db
ANAS <pawarate31@gmail.com>
2020-06-18 11:16:48 +0700

    Revert "Execise5_1"
    This reverts commit c66ee274547f5b429b012c97625abacfc5dfe59e.

--- Exercise5_1_Poramet_K.py
+++
@@ -1,6 +0,0 @@
-num1=int(input("Enter Number : "))
-num2=int(input("Enter Number : "))
-print(num1,"+",num2,"=",num1+num2)
-print(num1,"-",num2,"=",num1-num2)
-print(num1,"*",num2,"=",num1*num2)
-print(num1,"/",num2,"=",num1/num2)
